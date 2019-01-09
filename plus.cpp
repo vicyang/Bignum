ac131bfe8e630bcae289a69519835129836345a8
Vulkan <523066680@163.com>
2019-01-09 15:14:53 +0800

    Signed-off-by: Vulkan <523066680@163.com>

--- plus.cpp
+++ plus.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[] ) 
 {
-    std::cout<<"string"<<std::endl;
+    string a="99999999999999";
+    string b="123456789"; 
+    string c;
+    for (int i=0; i<a.length(); i++ )
+    {
+        cout << a[i] <<" ";
+    }
+    cout<<a<<endl;
     return 0;
 }
