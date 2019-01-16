03b0e7d3f5cc4738a605304fef7143801b992877
Vulkan <523066680@163.com>
2019-01-16 21:14:40 +0800

    not finish yet

--- plus.cpp
+++ plus.cpp
@@ -3,13 +3,18 @@ using namespace std;
 
 int main(int argc, char *argv[] ) 
 {
-    string a="99999999999999";
-    string b="123456789"; 
-    string c;
-    for (int i=0; i<a.length(); i++ )
+    string a="9876543210123456";
+    string b="123456789";
+    string c( a.length()+1, '0');
+
+    int len_a = a.length(), len_b = b.length();
+    int t, pool;
+
+    for (int iter = 0; iter < len_b; iter++ )
     {
-        cout << a[i] <<" ";
+        cout << b[len_b-iter-1] << a[len_a-iter-1] <<endl;
+        t = a.tointeger();
     }
-    cout<<a<<endl;
+    //cout<<c<<endl;
     return 0;
 }
