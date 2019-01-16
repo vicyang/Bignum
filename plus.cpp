e23ff458229890768bc3aee10c7211e6f81158ae
vic_notebook <523066680@163.com>
2019-01-16 22:30:05 +0800

    Signed-off-by: vic_notebook <523066680@163.com>

--- plus.cpp
+++ plus.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(int argc, char *argv[] ) 
 {
     string a="9876543210123456";
     string b="123456789";
-    string c( a.length()+1, '0');
+    //string c( a.length()+1, '0');
+    vector<short> c;
 
-    int len_a = a.length(), len_b = b.length();
-    int t, pool;
+    string cmd = "perl -Mbignum -e \"print " + a + "+" + b + "\"";
+    system( cmd.c_str() );
+    cout << endl;
 
-    for (int iter = 0; iter < len_b; iter++ )
+    int la = a.length(), lb = b.length();
+    int t, pool, ia, ib, ic;
+    pool = 0;
+    for (int iter = 0; iter < la; iter++ )
     {
-        cout << b[len_b-iter-1] << a[len_a-iter-1] <<endl;
-        t = a.tointeger();
+        ia = la-iter-1, ib = lb-iter-1;
+        t = a[ia]-'0' + pool;
+        if (ib >= 0) 
+            t = (a[ia]-'0') + (b[ib] - '0') + pool;
+        else
+            t = (a[ia]-'0') + pool;
+
+        c.push_back( t%10 );
+        pool = t/10;
+        cout << c[iter] << ",";
     }
     //cout<<c<<endl;
     return 0;
