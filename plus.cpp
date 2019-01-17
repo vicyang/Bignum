88e0598d71899feb1d0630e603c1fb3f9ef80014
Vulkan <523066680@163.com>
2019-01-17 10:29:33 +0800

    s_plus() 函数 改为引用传参，同时修剪冗余代码，提高效率。

--- plus.cpp
+++ plus.cpp
@@ -2,7 +2,7 @@
 #include <chrono>
 using namespace std;
 
-string s_plus(string a, string b);
+string s_plus(string& a, string& b);
 void check(string a, string b);
 
 int main(int argc, char *argv[] ) 
@@ -12,34 +12,36 @@ int main(int argc, char *argv[] )
     string b(10000, '9');
     string c=s_plus(a, b);
     //check(a, b);
-    for (int i = 0; i < 10000; i++) s_plus(a, b);
+    //耗时测试
+    for (int i = 0; i < 1000; i++) s_plus(a, b);
     auto end = chrono::system_clock::now();
     chrono::duration<double> diff = end-start;
-    //cout<<c<<endl;
+
+    //其他测试
+    a="99999", b="9999";
+    c=s_plus( a, b );
+    cout << c << endl;
     cout << "Time Used: " << diff.count() << " s" << endl;
-    
     return 0;
 }
 
 // plus 属于标准库函数的名称，所以加了前缀
-string s_plus(string a, string b)
+string s_plus(string& a, string& b)
 {
+    static int iter;
     string s( a.length(), '0');
     int la = a.length(), lb = b.length();
-    int t, pool = 0, ia, ib, ic;
-    for (int iter = 0; iter < la; iter++ )
+    int t, pool=0, ia=la-1, ib=lb-1;
+    for (iter = 0; iter < la; iter++ )
     {
-        ia = la-iter-1, ib = lb-iter-1, t = a[ia]-'0' + pool;
-        if (ib >= 0) 
-            t = (a[ia]-'0') + (b[ib] - '0') + pool;
+        if (ib >= 0)
+            t = (a[ia]-'0') + (b[ib--]-'0') + pool;
         else
             t = (a[ia]-'0') + pool;
-
-        s[ a.length()-iter-1 ] = t % 10 + '0';
-        pool = t/10;
+        s[ia--] = t%10 + '0', pool = t/10;
     }
 
-    if ( pool > 0 ) s.insert(0, 1, '1');
+    if ( pool > 0 ) s.insert(0, 1, pool+'0');
     return s;
 }
 
