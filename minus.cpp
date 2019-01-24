7b6734ce8ef878c9bc72e4660152f071329880c2
Vulkan <523066680@163.com>
2019-01-24 19:33:04 +0800

    vec_minus initial

--- minus.cpp
+++ minus.cpp
@@ -1,14 +1,44 @@
 #include <iostream>
 #include <chrono>
-#include <cstring>
+#include <vector>
 #include <cstdio>
 using namespace std;
 
 string s_minus(string a, string b);
-char* c_minus(char *a, char *b);
-
 int s_cmp( string &a, string &b );
-void check(string a, string b);
+
+void check(const vector<int> &va, const vector<int> &vb)
+{
+    string a("");
+    string b("");
+    for ( int it = 0; it < va.size(); it++ ) a += to_string(va[it]);
+    for ( int it = 0; it < vb.size(); it++ ) b += to_string(vb[it]);
+    string cmd = "perl -Mbignum -e \"print " + a + "-" + b + "\"";
+    system( cmd.c_str() );
+    cout << " <- check " << endl;
+}
+
+// 测试vector作为参数
+vector<int> vec_minus(const vector<int> &a, const vector<int> &b)
+{
+    static int ia; // iter
+    const int base = 1000000;
+    vector<int> c( a.size() );
+    //string s(20002, '0');
+    int t, cut=0, ib=b.size()-1;
+    int v, r;
+    for (ia = a.size()-1; ia >= 0; ia-- )
+    {
+        t = ib >= 0 ? (a[ia]) - (b[ib--]) + cut
+                    : (a[ia]) + cut;
+        if ( t < 0 )
+            t += base, cut = -1;
+        else 
+            cut = 0;
+        c[ia] = t;
+    }
+    return c;
+}
 
 int main(int argc, char *argv[] ) 
 {
@@ -16,73 +46,24 @@ int main(int argc, char *argv[] )
     chrono::duration<double> diff;
     string a(10000, '8');
     string b(10000, '9');
-    char* ch_a = (char *)malloc( 10001 * sizeof(char) );
-    char* ch_b = (char *)malloc( 10001 * sizeof(char) );
-    int len_a = 10000, len_b = 10000;
-    memset(ch_a, '9', len_a );
-    memset(ch_b, '8', len_b );
-    ch_a[len_a] = '\0';
-    ch_b[len_b] = '\0';
-    printf("len_a %d, len_b: %d\n", strlen( ch_a ), strlen( ch_b ) );
-
     string c;
     //耗时测试
     for (int i = 0; i < 20000; i++) s_minus(a, b);
     auto stage1 = chrono::system_clock::now();
     diff = stage1-stage0;
-    printf("Stage1, Time used: %f\n", diff.count());
+    cout << "s_minus, Time used: " << diff.count() << endl;
 
-    char* ch_c = c_minus(ch_a, ch_b);
-    //printf("%s\n", ch_c);
-    printf("%d\n", strlen(ch_c) );
-
-    //测试 c_minus 性能
-    {
-        for (int i = 0; i < 20000; i++) c_minus(ch_a, ch_b);
-        auto stage2 = chrono::system_clock::now();
-        diff = stage2-stage1;
-        printf("Stage2, Time used: %f\n", diff.count());
-    }
-
-    //其他测试 1
-    {
-        a="9999", b="19999";
-        printf("\nbefore a=%s, b=%s\n", a.c_str(), b.c_str());
-        c = s_minus( a, b );
-        printf("result = %s\n", c.c_str());
-        printf("after a=%s, b=%s\n", a.c_str(), b.c_str());
-        c = s_minus( b, a );
-        // check(a, b);
-    }
+    vector<int> va{123456, 654321};
+    vector<int> vb{ 93456, 924321};
+    check(va, vb);
+    vector<int> vc = vec_minus(va,vb);
+    for (int it = 0; it < vc.size(); it++) 
+        cout << vc[it] << "," ;
+    cout << endl;
 
     return 0;
 }
 
-char* c_minus(char* a, char* b)
-{
-    static int ia;
-    int la = strlen(a), lb = strlen(b), tl;
-    char* tp;
-    //如果 la < lb， 调换
-    if ( la < lb )
-        tp = a, a = b, b = tp, tl = la, la = lb, lb = tl;
-    else if ( la == lb && strcmp(a,b) < 0 )
-        tp = a, a = b, b = tp, tl = la, la = lb, lb = tl;
-
-    char* c = (char *)malloc( strlen(a) * sizeof(char) );
-
-    int t, cut=0, ib=lb-1, zero=0;
-    for (ia = la-1; ia >= 0; ia-- )
-    {
-        t = ib >= 0 ? (a[ia]-'0') - (b[ib--]-'0') - cut 
-                    : (a[ia]-'0') - cut;
-        cut = t < 0 ? 1 : 0;
-        c[ia] = t + '0' + cut*10;
-        c[ia] == '0' ? zero++ : zero=0;
-    }
-    //if (zero > 0) c.erase(0, zero);
-    return c;
-}
 
 // 大数减法 字符串操作, 暂时假设 a >= b
 // 传参使用副本拷贝，避免调换原数值
@@ -113,11 +94,4 @@ int s_cmp( string &a, string &b )
     if ( a.length() > b.length() ) return 1;
     else if ( a.length() < b.length() ) return -1;
     else return a.compare(b);
-}
-
-void check(string a, string b)
-{
-    string cmd = "perl -Mbignum -e \"print qq(check: ), " + a + "-" + b + "\"";
-    system( cmd.c_str() );
-    cout << endl;
 }
