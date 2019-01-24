db1d820ab11b1f9eefd56583495735353210d7fb
Vulkan <523066680@163.com>
2019-01-24 17:47:10 +0800

    向量存储, base为10000，初步验证数值正确（细节0问题未考虑）

--- plus.cpp
+++ plus.cpp
@@ -7,7 +7,7 @@ using namespace std;
 string s_plus(const string& a, const string& b);
 string sn_plus(const string& a, const string& b);
 string vec_plus(const vector<int>& a, const vector<int>& b);
-void check(string a, string b);
+void check(const string &a, const string &b);
 
 int main(int argc, char *argv[] ) 
 {
@@ -23,16 +23,18 @@ int main(int argc, char *argv[] )
     diff = stage1-stage0;
     cout << "Stage 1, Time Used: " << diff.count() << " s" << endl;
 
-    vector<int> va(20000, 9);
-    vector<int> vb(20000, 9);
+    check(string (20, '9'), string(20, '9') );
+    stage1 = chrono::system_clock::now();
+
+    vector<int> va(5, 9999);
+    vector<int> vb(5, 9999);
     string sc = vec_plus(va, vb);
-    //cout << sc << endl;
+    cout << sc << endl;
     //耗时测试
     for (int i = 0; i < 1000; i++) vec_plus(va, vb);
     auto stage2 = chrono::system_clock::now();
     diff = stage2-stage1;
     cout << "Stage 2, Time Used: " << diff.count() << " s" << endl;
-
 
     string na(20000, 9);
     string nb(20000, 9);
@@ -49,6 +51,30 @@ int main(int argc, char *argv[] )
     c=s_plus( a, b );
     cout << c << endl;
     return 0;
+}
+
+// 测试vector作为参数
+string vec_plus(const vector<int> &a, const vector<int> &b)
+{
+    static int ia; // iter
+    const int base = 10000;
+    string s;
+    //string s(20002, '0');
+    int t, pool=0, ib=b.size()-1;
+    int v, r;
+    for (ia = a.size()-1; ia >= 0; ia-- )
+    {
+        t = ib >= 0 ? (a[ia]) + (b[ib--]) + pool
+                    : (a[ia]) + pool;
+        v = t % base, pool = t/base;
+        s.insert( 0, to_string(v) );
+        // s[ia*4] = v/1000 + '0', v=v%1000;
+        // s[ia*4+1] = v/100 + '0', v=v%100;
+        // s[ia*4+2] = v/10 + '0', v=v%10;
+        // s[ia*4+3] = v + '0';
+    }
+    if ( pool > 0 ) s.insert(0, 1, pool+'0');
+    return s;
 }
 
 // plus 属于标准库函数的名称，所以加了前缀
@@ -83,25 +109,9 @@ string sn_plus(const string& a, const string& b)
     return s;
 }
 
-// 测试vector作为参数
-string vec_plus(const vector<int> &a, const vector<int> &b)
-{
-    static int ia; // iter
-    string s( a.size(), '0');
-    int t, pool=0, ib=b.size()-1;
-    for (ia = a.size()-1; ia >= 0; ia-- )
-    {
-        t = ib >= 0 ? (a[ia]) + (b[ib--]) + pool
-                    : (a[ia]) + pool;
-        s[ia] = t%10 + '0', pool = t/10;
-    }
-    if ( pool > 0 ) s.insert(0, 1, pool+'0');
-    return s;
-}
-
-void check(string a, string b)
+void check(const string &a, const string &b)
 {
     string cmd = "perl -Mbignum -e \"print " + a + "+" + b + "\"";
     system( cmd.c_str() );
-    cout << endl;
+    cout << " <- check " << endl;
 }
