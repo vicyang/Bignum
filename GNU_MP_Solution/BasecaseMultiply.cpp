431d702a4140f5a25706fd9caff97de6f293822a
Vulkan <523066680@163.com>
2019-02-01 21:44:51 +0800

    初步OK

--- GNU_MP_Solution/BasecaseMultiply.cpp
+++ GNU_MP_Solution/BasecaseMultiply.cpp
@@ -8,7 +8,8 @@ typedef unsigned long long ULL;
 string vec2str( const vector<ULL> &vec );
 vector<ULL> vec_plus(const vector<ULL> &a, const vector<ULL> &b);
 vector<ULL> BasecaseMultiply( const vector<ULL>& a, const vector<ULL>& b);
-vector<ULL> mp_single( const vector<ULL>& a, int b);
+vector<ULL> vec_mp_single( const vector<ULL>& a, int b);
+void shift( vector<ULL>& vec, int n );
 string zeros(10000, '0');
 
 const unsigned long long BASE = 1000000;
@@ -18,18 +19,14 @@ int main(int argc, char *argv[] )
 {
     vector<ULL> a{123, 666666};
     vector<ULL> b{456, 999999};
-    // string c = BasecaseMultiply(a, b);
-    //cout << c;
-
     vector<ULL> c;
     c = BasecaseMultiply( a, b );
     cout << vec2str(c);
 
-
     return 0;
 }
 
-vector<ULL> mp_single( const vector<ULL>& a, int b)
+vector<ULL> vec_mp_single( const vector<ULL>& a, int b)
 {
     vector<ULL> c( a.size() );
     if ( b == 0 ) { return vector<ULL>{0}; }
@@ -42,17 +39,25 @@ vector<ULL> mp_single( const vector<ULL>& a, int b)
     return c;
 }
 
+// 参数：向量引用、 前移的段数（也许需要考虑vec为0的情况）
+void shift( vector<ULL>& vec, int n )
+{
+    for (int i = 0; i < n; i++) vec.push_back(0);
+}
+
+// 假设 b.size() >= a.size()
 vector<ULL> BasecaseMultiply( const vector<ULL>& a, const vector<ULL>& b)
 {
     vector<ULL> c;
     vector<ULL> t;
-    int bi = b.size() - 1;
-    c = mp_single( a, b[bi--] );
-    // while ( bi-- >= 0 )
-    // {
-    //     c = mp_single(a, b[bi--]);
-    // }
-
+    int bi = b.size() - 1, append = 1;
+    c = vec_mp_single( a, b[bi--] );
+    while ( bi >= 0 )
+    {
+        t = vec_mp_single(a, b[bi--]);
+        shift(t, append++);
+        c = vec_plus(c, t);
+    }
     return c;
 }
 
