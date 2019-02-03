6d20371f577fa4a1569943c933eed530c6d4138f
Vulkan <523066680@163.com>
2019-02-03 12:40:16 +0800

    sqrt_vector version initial

--- sqrt_decimal.cpp
+++ sqrt_decimal.cpp
@@ -8,12 +8,15 @@
 #include <vector>
 #include <chrono>
 using namespace std;
+typedef unsigned long long int ULL;
+const ULL BASE = 100000000;
+const ULL LEN = 8;
 
 string s_mp_single(const string& a, const string& b);
 string s_plus(const string& a, const string& b);
 string s_minus(const string& a, const string& b);
 int s_cmp(const string &a, const string &b );
-string sqrt_decimal(const string &num, int precision);
+string sqrt_decimal(ULL num, int precision);
 
 vector<int> vec_mp_single(const vector<int> &a, const vector<int> &b);
 vector<int> vec_minus(const vector<int> &a, const vector<int> &b);
@@ -24,7 +27,9 @@ int main(int argc, char *argv[] )
 {
     chrono::duration<double> diff;
     auto stage0 = chrono::system_clock::now();
-    sqrt_decimal("2", 10000);
+
+    vector<ULL> num{2};
+    sqrt_decimal(num[0], 100);
     auto stage1 = chrono::system_clock::now();
     
     diff = stage1-stage0;
@@ -32,94 +37,13 @@ int main(int argc, char *argv[] )
     return 0;
 }
 
-string sqrt_decimal(const string &num, int precision)
+string sqrt_decimal(ULL num, int precision)
 {
-    int BASE = 10^6;
-    static string itoc("0123456789");
-    static vector<string> itos{"0","1","2","3","4","5","6","7","8","9"};
-
-    int len = num.length(), mod = len % 2, skip = 2 - mod;
-    string target = num.substr(0, skip);
-    string tnum = num.substr(skip);
-
     bool dec_loop = 1;
     int prec = 0, est, mp, mid, cmp, dbmid;
-    string base("");
 
-    string s_mp;
 
-    while (dec_loop && prec < precision )
-    {
-        if ( s_cmp(target, base + "0") < 0 ) {
-            mid = 0, est = 0, s_mp = "0";
-        } else {
-            //评估下一个数字（est）
-            if ( base.length() > 5 )
-                est = stoi(target.substr(0, 6)) / stoi(base.substr(0, 5));
-            else
-            {
-                est = 9;  // 如果遍历0-9均没有超越target，est取值为9
-                for (int i = 0; i <= 9; i++ )
-                {
-                    mp = stoi(base+itoc[i]) * i;
-                    if ( mp > stoi(target) ) { est = i-1; break; }
-                }
-            }
-        }
 
-        //如果估值大于10，考虑取9还是取首位数
-        if ( est >= 10 ) 
-            est = target.length() > (base.length()+1) ? 9 : est/10;
-
-        mid = est;
-        s_mp = s_mp_single( base+itoc[mid], itos[mid] );
-        cmp = s_cmp( s_mp, target );
-
-        //如果 最后乘出来的结果大于 target
-        if ( cmp > 0 ) {
-            //cout << "what?";
-            mid -= 1;
-            s_mp = s_mp_single( base+itoc[mid], itos[mid] );
-        }
-        
-        cout << mid;
-        //printf(" est %d, tg %16s, base, %s, s_mp %s, \n", est, target.c_str(), base.c_str(), s_mp.c_str());
-
-        // 更新 target 值
-        target = s_minus(target, s_mp);
-        if (tnum.length() == 0 ) {
-            //if ( target.compare("00") == 0 ) break;
-            if ( target.compare("0") == 0 ) break;
-            if ( cmp == 0 ) break;
-            target += "00";
-            prec += 1;
-            if (prec == 1) cout << ".";
-        } else {
-            if ( target.compare("0") == 0 ) {
-                target = tnum.substr(0,2);
-                if ( target[0] == '0' ) target.erase(0,1);
-            } else {
-                target += tnum.substr(0,2);
-            }
-            tnum = tnum.substr(2);
-        }
-
-        //更新 base - 基数
-        if ( base.length() == 0 ) {
-            base = to_string(mid*2);
-        } else {
-            // mid*2 有可能是两位数，确保base至少有1位（s_plus假设a长度大于等于b）
-            dbmid = mid*2;
-            if (dbmid > 9)
-                if ( base.back() < '9' )
-                    base.back() += 1, base += (dbmid-10)+'0';
-                else
-                    cerr << "what's wrong with you?" << endl;
-            else
-                base += dbmid+'0';
-        }
-        
-    }
     cout << endl;
     return "";
 }
