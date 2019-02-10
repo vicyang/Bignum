b92332545cb891221d4918d728100d3dc93e8dfa
Vulkan <523066680@163.com>
2019-02-10 16:07:36 +0800

    modify, something wrong

--- GNU_MP_Solution/BasecaseDivRem.cpp
+++ GNU_MP_Solution/BasecaseDivRem.cpp
@@ -1,6 +1,7 @@
 // BasecaseMultiply - C++ Vector
+// Notes: 实现该方法应先实现 有符号的加减函数。
 // 523066680/vicyang
-// 2019-01
+// 2019-02
 
 #include <iostream>
 #include <string>
@@ -9,21 +10,22 @@
 using namespace std;
 using namespace std::chrono;
 typedef unsigned long long ULL;
+typedef long long LL;
 
-string vec2str( const vector<ULL> &vec );
-vector<ULL> BasecaseMultiply( const vector<ULL>& a, const vector<ULL>& b);
-vector<ULL> vec_mp_single(const vector<ULL> &a, const vector<ULL> &b);
-vector<ULL> vec_plus(const vector<ULL> &a, const vector<ULL> &b);
-vector<ULL> vec_minus(const vector<ULL> &a, const vector<ULL> &b);
-int vec_cmp(vector<ULL> &a, vector<ULL> &b);
-int vec_accum(vector<ULL> &a, vector<ULL> &b, int bbegin);
+string vec2str( const vector<LL> &vec );
+vector<LL> BasecaseMultiply( const vector<LL>& a, const vector<LL>& b);
+vector<LL> vec_mp_single(const vector<LL> &a, const vector<LL> &b);
+vector<LL> vec_plus(const vector<LL> &a, const vector<LL> &b);
+vector<LL> vec_minus(const vector<LL> &a, const vector<LL> &b);
+int vec_cmp(const vector<LL> &a, const vector<LL> &b);
+int vec_accum(vector<LL> &a, vector<LL> &b, int bbegin);
 
 void time_used(system_clock::time_point& time_a);
 
-const ULL BASE = 100000000;
+const LL BASE = 100000000;
 const int MAXLEN = 8;
 
-void check(const vector<ULL> &va, const vector<ULL> &vb, const string &op)
+void check(const vector<LL> &va, const vector<LL> &vb, const string &op)
 {
     string a = vec2str(va);
     string b = vec2str(vb);
@@ -33,18 +35,18 @@ void check(const vector<ULL> &va, const vector<ULL> &vb, const string &op)
 }
 
 // 参数：向量引用、 前移的段数（也许需要考虑vec为0的情况）
-void shift( vector<ULL>& vec, int n )
+void shift( vector<LL>& vec, int n )
 {
     for (int i = 0; i < n; i++) vec.push_back(0);
 }
 
-vector<ULL> BasecaseDivRem( const vector<ULL> &a, const vector<ULL> &b )
+vector<LL> BasecaseDivRem( const vector<LL> &a, const vector<LL> &b )
 {
     int asize = a.size(), bsize = b.size();
     int n = b.size(), m = a.size() - b.size();
-    vector<ULL> tb = b;
-    vector<ULL> ta = a;
-    vector<ULL> q(m+1);
+    vector<LL> tb = b;
+    vector<LL> ta = a;
+    vector<LL> q(m+1);
     shift(tb, m);
     if ( vec_cmp(ta, tb) > -1 ) {
         q[m] = 1;
@@ -53,24 +55,40 @@ vector<ULL> BasecaseDivRem( const vector<ULL> &a, const vector<ULL> &b )
         q[m] = 0;
     }
 
-    ULL tq;
-    vector<ULL> tv;
+    cout << "ta befor fo: " << vec2str(ta) << endl;
+    LL tq;
+    vector<LL> tv;
     for ( int j = m-1; j >= 0; j-- )
     {
-        tq = (ta[asize-(n+j)-1] + ta[asize-(n+j)])/tb[0];
+        tq = (ta[asize-(n+j)-1]*BASE + ta[asize-(n+j)])/b[0];
+        cout << "tq: " << tq << endl;
+
         q[j] = tq < BASE - 1 ? tq : BASE - 1;
         // ta = vec_minus(ta, tb);
-        tv = vec_mp_single(b, vector<ULL>{q[j]} );
+        tv = vec_mp_single( b, vector<LL>{q[j]} );
         shift(tv, j);
         ta = vec_minus( ta, tv );
-        
+        cout << "ta before while: " << vec2str(ta) << endl;
 
-        cout << vec2str(ta) << "," << q[j] << endl;
+        tb = b;
+        while ( ta[0] < 0 )
+        {
+            q[j] -= 1;
+            shift(tb, 1);
+            cout << "before: " << vec2str(ta) << "," << vec2str(tb) << endl;
+            ta[0] *= -1;
+            ta = vec_minus(tb, ta);
+            cout << "while: " << vec2str(ta) << "," << vec2str(tb) << endl;
+        }
+
+        cout << "next" <<endl;
         //cout << ta[asize-(n+j)-1] << ",";
     }
 
+    cout << vec2str( q ) << endl;
+
     cout << endl;
-    return vector<ULL>{0};
+    return vector<LL>{0};
 }
 
 int main(int argc, char *argv[] ) 
@@ -78,10 +96,10 @@ int main(int argc, char *argv[] )
     system_clock::time_point start;
     start = system_clock::now();
 
-    vector<ULL> a{9999, 0, 12345678};
-    //vector<ULL> b{58765432};
-    vector<ULL> b{5876};
-    vector<ULL> c;
+    vector<LL> a{9999, 0, 12345678};
+    //vector<LL> b{58765432};
+    vector<LL> b{5876};
+    vector<LL> c;
     check( a, b, "/" );
     BasecaseDivRem(a, b);
 
@@ -92,12 +110,12 @@ int main(int argc, char *argv[] )
 }
 
 // 在原来的基础上叠加数值
-int vec_accum(vector<ULL> &a, vector<ULL> &b, int bbegin)
+int vec_accum(vector<LL> &a, vector<LL> &b, int bbegin)
 {
     static int ia; // iter
-    register ULL* pa = a.data();
-    register ULL* pb = b.data();
-    ULL t, pool=0;
+    register LL* pa = a.data();
+    register LL* pb = b.data();
+    LL t, pool=0;
     register int ib = b.size()-1;
     for (ia = a.size()-1; ia >= 0; ia-- )
     {
@@ -106,21 +124,21 @@ int vec_accum(vector<ULL> &a, vector<ULL> &b, int bbegin)
         pa[ia] = t % BASE, pool = t/BASE;
         if ( pool == 0 and ib < bbegin ) break;
     }
-    if ( pool > 0ULL ) pa[ia] = pool;
+    if ( pool > 0LL ) pa[ia] = pool;
     return ia;
 }
 
 // vec multiply to one word
-vector<ULL> vec_mp_single(const vector<ULL> &a, const vector<ULL> &b)
+vector<LL> vec_mp_single(const vector<LL> &a, const vector<LL> &b)
 {
-    register const ULL *pa = a.data();
-    register const ULL *pb = b.data();
+    register const LL *pa = a.data();
+    register const LL *pb = b.data();
     static int ia; // iter
     //如果b是0，直接返回0
-    if ( b[0] == 0 ) return vector<ULL>{0};
-    vector<ULL> c( a.size() );
-    register ULL *pc = c.data();
-    ULL t, pool=0, ib=b.size()-1;
+    if ( b[0] == 0 ) return vector<LL>{0};
+    vector<LL> c( a.size() );
+    register LL *pc = c.data();
+    LL t, pool=0, ib=b.size()-1;
     for (ia = a.size()-1; ia >= 0; ia-- )
     {
         t = pa[ia] * pb[0] + pool;
@@ -130,13 +148,13 @@ vector<ULL> vec_mp_single(const vector<ULL> &a, const vector<ULL> &b)
     return c;
 }
 
-vector<ULL> vec_plus(const vector<ULL> &a, const vector<ULL> &b)
+vector<LL> vec_plus(const vector<LL> &a, const vector<LL> &b)
 {
     static int ia; // iter
-    vector<ULL> c( a.size() );
-    register const ULL *pa = a.data();
-    register const ULL *pb = b.data();
-    register ULL *pc = c.data();
+    vector<LL> c( a.size() );
+    register const LL *pa = a.data();
+    register const LL *pb = b.data();
+    register LL *pc = c.data();
     int t, pool=0, ib=b.size()-1;
     int v, r;
     for (ia = a.size()-1; ia >= 0; ia-- )
@@ -149,18 +167,27 @@ vector<ULL> vec_plus(const vector<ULL> &a, const vector<ULL> &b)
     return c;
 }
 
-// vec_minus 设 a.len > b.len
-vector<ULL> vec_minus(const vector<ULL> &a, const vector<ULL> &b)
+//带符号的减法（待更）
+vector<LL> vec_minus(const vector<LL> &a, const vector<LL> &b)
 {
-    register int ia;
-    register int ib = b.size()-1;
-    vector<ULL> c( a.size() );
-    register const ULL *pa = a.data();
-    register const ULL *pb = b.data();
-    register ULL *pc = c.data();
-    int cut=0, zero=0;
-    long long int t; // t可能为负数
-    for (ia = a.size()-1; ia >= 0; ia-- )
+    static int ia, asize, bsize; 
+    LL sign = 1;
+    register const LL *pa, *pb;
+    vector<LL> c;
+    //如果 a < b，指针调换，sign 为 -1
+    if ( vec_cmp(a, b) == -1 ) {
+        c.assign(b.size(), 0);
+        pa = b.data(), pb = a.data(), sign = -1;
+        asize = b.size(), bsize = a.size();
+    } else {
+        c.assign(a.size(), 0);
+        pa = a.data(), pb = b.data();
+        asize = a.size(), bsize = b.size();
+    }
+    
+    register LL *pc = c.data();
+    LL t, cut=0, ib=bsize-1, zero=0;
+    for (ia = asize-1; ia >= 0; ia-- )
     {
         t = ib >= 0 ? (pa[ia]) - (pb[ib--]) + cut
                     : (pa[ia]) + cut;
@@ -169,17 +196,17 @@ vector<ULL> vec_minus(const vector<ULL> &a, const vector<ULL> &b)
         pc[ia] = t;
     }
     c.erase(c.begin(), c.begin()+zero);
+    c[0] *= sign;
     return c;
 }
 
-int vec_cmp(vector<ULL> &a, vector<ULL> &b)
+int vec_cmp(const vector<LL> &a, const vector<LL> &b)
 {
-    register ULL* pa = a.data();
-    register ULL* pb = b.data();
+    register const LL* pa = a.data();
+    register const LL* pb = b.data();
     int len_a = a.size(), len_b= b.size();
     if ( len_a > len_b ) { return 1; }
     else if ( len_a < len_b ) { return -1; }
-
     register int it = 0;
     for ( ; it < len_a; it++ ) {
              if ( pa[it] > pb[it] ) { return 1; }
@@ -188,7 +215,7 @@ int vec_cmp(vector<ULL> &a, vector<ULL> &b)
     return 0;
 }
 
-string vec2str( const vector<ULL> &vec )
+string vec2str( const vector<LL> &vec )
 {
     string s("");
     s += to_string( vec[0] );
