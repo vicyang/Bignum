8f887d2c8b5ed7b4e0d92ed432b1a338ce030d26
Vulkan <523066680@163.com>
2019-03-02 17:13:48 +0800

    四则运算+重载，起点

---
+++ overload.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+typedef long long int LL;
+
+struct bigint {
+    vector<LL> n;
+    int sign;
+
+    void bigint( vector<LL> n, int sign ) {}
+};
+
+int main(int argc, char *argv[] ) 
+{
+    //bigint v{ vector };
+    return 0;
+}
